793926c6c4a438e0f82a9172a6050d71c79572f9
carolinipimenta <carolinilaila@hotmail.com>
2024-04-29 21:33:19 -0300

    feat: create modal content and update import assets

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,20 +7,42 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
+        <div onclick="openModal()">
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
 
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
 
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
-        </li>
+                    <img src="${pokemon.photo}"
+                        alt="${pokemon.name}">
+                </div>
+            </li>
+        </div>
     `
+}
+
+// Função para abrir o modal
+function openModal() {
+    var modal = document.getElementById("modal");
+    modal.style.display = "block";
+}
+
+// Função para fechar o modal
+function closeModal() {
+    var modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
+
+// Fechar o modal clicando fora da área do modal
+window.onclick = function(event) {
+    var modal = document.getElementById("modal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
 
 function loadPokemonItens(offset, limit) {
