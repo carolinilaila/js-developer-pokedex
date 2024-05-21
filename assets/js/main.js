cbbc67d40095e5e02038271e197fe075da36f2a8
carolinipimenta <carolinilaila@hotmail.com>
2024-05-21 08:21:24 -0300

    feat: pokemon details criado [bug] arrumar pokemonPosition quando o loadMoreButon for chamado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+const pokemonModal = document.getElementById('pokemonModal')
 
 const maxRecords = 151
 const limit = 10
@@ -7,7 +8,7 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <div onclick="openModal()">
+        <div onclick="openModal(event)" class="modal-click" id="pokemon-modal" data-value="${pokemon.number - 1}">
             <li class="pokemon ${pokemon.type}">
                 <span class="number">#${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
@@ -25,24 +26,16 @@ function convertPokemonToLi(pokemon) {
     `
 }
 
-// Função para abrir o modal
-function openModal() {
-    var modal = document.getElementById("modal");
-    modal.style.display = "block";
-}
-
-// Função para fechar o modal
-function closeModal() {
-    var modal = document.getElementById("modal");
-    modal.style.display = "none";
-}
-
-// Fechar o modal clicando fora da área do modal
-window.onclick = function(event) {
-    var modal = document.getElementById("modal");
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+function convertPokemonDetailToSection(pokemon)  {
+    return `
+        <section class="pokemon>
+            <span class="name">${pokemon.name}</span>
+            <ol class="types">
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+            </ol>
+            <img src="${pokemon.photo}" alt="${pokemon.name}">
+        </section>
+    `
 }
 
 function loadPokemonItens(offset, limit) {
@@ -61,9 +54,54 @@ loadMoreButton.addEventListener('click', () => {
     if (qtdRecordsWithNexPage >= maxRecords) {
         const newLimit = maxRecords - offset
         loadPokemonItens(offset, newLimit)
-
         loadMoreButton.parentElement.removeChild(loadMoreButton)
     } else {
         loadPokemonItens(offset, limit)
+        // TODO: arrumar pokemonPosition quando clicar no loadMoreButton
+        loadPokemonDetails()
     }
-})
+})  
+
+// Função para abrir o modal
+function openModal(event) {
+    const modal = document.getElementById("modal");
+    modal.style.display = "block";
+    const clickedPokemon = event.currentTarget;
+    const pokemonPosition = clickedPokemon.dataset.value
+    loadPokemonDetails(pokemonPosition)
+}
+
+// Função para fechar o modal
+function closeModal() {
+    const modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
+
+// Fechar o modal clicando fora da área do modal
+window.onclick = function(event) {
+    var modal = document.getElementById("modal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function loadPokemonDetails(pokemonPosition) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        let newPokemon = []
+        console.log(pokemons)
+        pokemons.forEach((pokemon) => {
+            newPokemon.push({ number: pokemon.number, name: pokemon.name, photo: pokemon.photo, types: pokemon.types })
+        })
+        
+        if (pokemonPosition >= 0 && pokemonPosition < newPokemon.length) {
+            const pokemonDetails = newPokemon[pokemonPosition];
+            console.log(pokemonDetails);
+            const newHtml = convertPokemonDetailToSection(pokemonDetails);
+            pokemonModal.innerHTML = newHtml;
+            return pokemonDetails;
+        } else {
+            console.log("Erro: Pokemon não existe!")
+            return null
+        }
+    })
+}
